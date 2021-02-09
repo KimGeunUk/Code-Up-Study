94ecc6072c4944b7594b5ce9b05be9acc2c4e67d
KimGeunUk <74355042+KimGeunUk@users.noreply.github.com>
2021-02-09 19:24:31 +0900

    Add files via upload

---
+++ 1024.py
@@ -0,0 +1,12 @@
+#단어를 1개 입력받는다.
+#입력받은 단어(영어)의 각 문자를
+#한줄에 한 문자씩 분리해 출력한다.
+#ex) BOY -> 'B', 'O', 'Y'
+
+a = input()
+for i in range(len(a)):
+  print("'{}'".format(a[i]))
+
+b = input()
+for i in b:
+  print("'" + i + "'")
