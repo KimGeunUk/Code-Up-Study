b35c29e101fbb31f36049f0d30d07c790d758503
KimGeunUk <74355042+KimGeunUk@users.noreply.github.com>
2021-02-24 20:21:24 +0900

    1082.py

---
+++ 1082.py
@@ -0,0 +1,12 @@
+#A, B, C, D, E, F 중 하나가 입력될 때,
+#1부터 F까지 곱한 16진수 구구단의 내용을 출력해보자.
+
+a = input()
+
+b = int((input(),16))
+
+for i in range(1, 16):
+  print("{}*{}={}".format(a,hex(i)[2:].upper(),hex(i*int(a,16))[2:].upper()))
+
+#for i in range(1,16):
+#  print("%X*%X=%X" %(b,i,b*i))
